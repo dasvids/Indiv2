1536d2af779191c01e4dffd78e2cb303538bf329
grchs <nedorubartem@gmail.com>
2022-10-27 22:14:23 +0300

    some fixes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,47 +40,58 @@
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(13, 13);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(775, 401);
+            this.pictureBox1.Size = new System.Drawing.Size(1239, 626);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(78, 420);
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(74, 645);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Magic";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(159, 420);
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(230, 645);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(150, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(15, 421);
+            this.checkBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(13, 645);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "inTime";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.UseVisualStyleBackColor = false;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
--- Form1.cs
+++ Form1.cs
@@ -24,71 +24,60 @@ namespace Indiv2
             color = Color.Black;
             pen = new Pen(color);
         }
-        //lowest point
+        //lowest point (max coorinates point of picturebox)
         Point p0 = new Point();
         
-        //points by user's clicks  
+        //points by user's clicks
         List<Point> points = new List<Point>();      
         
-        int iter = 0; //counter for Graham
+        //iteration
+        int iter = 0; 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
-            g.FillEllipse(Brushes.Black, e.X - 2, e.Y - 2, 4, 4); //drawing point
-            points.Add(e.Location);  //adding point to list 
-            iter++; //increase iteration 
+            //Draw point
+            g.FillEllipse(Brushes.Red, e.X - 3, e.Y - 3, 6, 6);
+            //adding point to list 
+            points.Add(e.Location);
+            //increase iteration 
+            iter++; 
+            //updationg changes 
             pictureBox1.Invalidate();
-
-            if (iter >= 3 && checkBox1.Checked) //working only if 3 or more points
+            //if points 3 or more doing graham scan
+            if (iter >= 3 && checkBox1.Checked) 
             {
+                //result of Graham Scan
                 List<Point> S = Graham(points); 
-                g.FillPolygon(Brushes.SandyBrown, S.ToArray());
-                points.ForEach(p => g.FillEllipse(Brushes.Black, p.X - 2, p.Y - 2, 4, 4)); //drawing points again, to overlay polygon
+                //drawing polygon by points, selected by graham scan
+                g.FillPolygon(Brushes.Thistle, S.ToArray());
+                //drawing points again, to overlay polygon
+                points.ForEach(p => g.FillEllipse(Brushes.Red, p.X - 3, p.Y - 3, 6, 6)); 
             }
         }
-        /// <summary>
-        /// dirrection, clockwise or counterclockwise
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="c"></param>
-        /// <returns> 0 if clockwise turn, 1 if counter-otherwise</returns>
+        // 0 if clockwise turn, 1 if counter-otherwise
         private static bool ccw(Point a, Point b, Point c) 
         {
+            //return ((b.X - a.X) * (c.Y - a.Y)) > ((b.Y - a.Y) * (c.X - a.X));
             return ((b.X - a.X) * (c.Y - a.Y)) > ((b.Y - a.Y) * (c.X - a.X));
         }
-        List<Point> Graham(List<Point> Q) //Graham scan
+        List<Point> Graham(List<Point> G) //Graham scan
         {
             //p0 is min point by y and max by x
-            p0 = Q.OrderByDescending(p => p.Y).ThenByDescending(p => p.X).First(); // min point is max of picture box height abd width                       
-            
-            //groupby by polar angle relative p0
-            List<Point> P = Q.Where(p => p != p0).OrderBy(x => alpha(x)).ToList();
-
-            //failed up with stack ):, doing reverce loop in list instead ¯\_(ツ)_/¯ 
-            /*Stack<Point> stack = new Stack<Point>();
-            Q.ForEach(p =>
+            p0 = G.OrderByDescending(p => p.Y).ThenByDescending(p => p.X).First(); // min point is max of picture box height abd width          
+            //groupby by polar angle relative p0 (sorting)
+            List<Point> P = G.Where(p => p != p0).OrderBy(x => alpha(x)).ToList();
+            // stack of final points 
+            Stack<Point> stack = new Stack<Point>();
+            //inserting min point
+            stack.Push(p0);
+            P.ForEach(p =>
             {
-                while (stack.Count > 1 && !ccw(next_to_top(stack), top(stack), p))
+                while (stack.Count > 1 && ccw(next_to_top(stack), top(stack), p))
+                {
                     stack.Pop();
+                }
                 stack.Push(p);
             });
-
-            return stack.ToList();*/
-
-            List<Point> h = new List<Point>();
-            h.Add(p0);
-            
-            int t = h.Count + 1;
-            for (int i = P.Count() - 1; i >= 0; i--)
-            {
-                Point p = P[i];
-                while (h.Count >= t && !ccw(h[h.Count - 2], h[h.Count - 1], p))
-                {
-                    h.RemoveAt(h.Count - 1);
-                }
-                h.Add(p);
-            }
-            return h;
+            return stack.ToList();
         }
 
         private Point next_to_top(Stack<Point> stack)
@@ -99,7 +88,7 @@ namespace Indiv2
             stack.Push(p);
             return next;
         }
-
+      
         private Point top(Stack<Point> stack)
         {
             Point p = stack.Pop();
@@ -118,8 +107,8 @@ namespace Indiv2
             if (points.Count >= 3) //working only if 3 or more points
             {
                 List<Point> S = Graham(points);
-                g.FillPolygon(Brushes.SandyBrown, S.ToArray());
-                points.ForEach(p => g.FillEllipse(Brushes.Black, p.X - 2, p.Y - 2, 4, 4)); //drawing points again, to overlay polygon
+                g.FillPolygon(Brushes.Thistle, S.ToArray());
+                points.ForEach(p => g.FillEllipse(Brushes.Red, p.X - 3, p.Y - 3, 6, 6)); //drawing points again, to overlay polygon
             }
             pictureBox1.Invalidate();
         }
